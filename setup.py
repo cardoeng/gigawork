dba077298b2aa88b1d03c2f8d846d9020445b88a
Guillaume Cardoen <G.Cardoen@outlook.com>
2024-03-25 10:38:02 +0100

    feat: bump version to 1.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(
 
 setup(
     name=NAME,
-    version="1.2.0",
+    version="1.3.0",
     license="LGPLv3",
     author="Guillaume Cardoen",
     url="https://github.com/cardoeng/gigawork",
